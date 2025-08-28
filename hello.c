97975a415350f1f32defa750cfcf5c2fe5c861df
Lily Dyer <jdyer9@huskers.unl.edu>
2025-08-28 10:00:00 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Lily Dyer
+ * Email: jdyer9@huskers.unl.edu
+ * Date: 2025/08/28
+ *
+ * Basic program to print my name!
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Lily Dyer\n");
+
+}
